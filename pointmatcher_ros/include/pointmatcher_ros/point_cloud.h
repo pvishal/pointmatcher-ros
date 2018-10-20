a985590549147197feccb90aa980da2ec4255bf4
Vishal Prabhu <vishal.prabhu@gmail.com>
2018-10-19 17:47:17 -0700

    Simplified the LaserScan to Datapoints conversion to not require a tf listener

--- pointmatcher_ros/include/pointmatcher_ros/point_cloud.h
+++ pointmatcher_ros/include/pointmatcher_ros/point_cloud.h
@@ -8,23 +8,23 @@
 
 namespace ros
 {
-	struct Time;
+struct Time;
 };
 namespace tf
 {
-	struct TransformListener;
+struct TransformListener;
 };
 
 namespace PointMatcher_ros
 {
-	template<typename T>
-	typename PointMatcher<T>::DataPoints rosMsgToPointMatcherCloud(const sensor_msgs::PointCloud2& rosMsg, const bool isDense=false);
-	
-	template<typename T>
-	typename PointMatcher<T>::DataPoints rosMsgToPointMatcherCloud(const sensor_msgs::LaserScan& rosMsg, const tf::TransformListener* listener = 0, const std::string& fixed_frame = "/world", const bool force3D = false, const bool addTimestamps=false, const bool addObservationDirection=false);
-	
-	template<typename T>
-	sensor_msgs::PointCloud2 pointMatcherCloudToRosMsg(const typename PointMatcher<T>::DataPoints& pmCloud, const std::string& frame_id, const ros::Time& stamp);
-}; // PointMatcher_ros
+template <typename T>
+typename PointMatcher<T>::DataPoints rosMsgToPointMatcherCloud(const sensor_msgs::PointCloud2 &rosMsg, const bool isDense = false);
+
+template <typename T>
+typename PointMatcher<T>::DataPoints rosMsgToPointMatcherCloud(const sensor_msgs::LaserScan &rosMsg, const bool addTimestamps = false);
+
+template <typename T>
+sensor_msgs::PointCloud2 pointMatcherCloudToRosMsg(const typename PointMatcher<T>::DataPoints &pmCloud, const std::string &frame_id, const ros::Time &stamp);
+}; // namespace PointMatcher_ros
 
 #endif //__POINTMATCHER_ROS_POINT_CLOUD_H
